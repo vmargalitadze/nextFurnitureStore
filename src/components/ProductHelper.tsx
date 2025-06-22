685c9556beeadb7f93e7b88d1a7fae1ca6b5ee03
vaqsii <vaqsii23@gmail.com>
2025-06-22 16:15:06 +0400

    helper

--- src/components/ProductHelper.tsx
+++ src/components/ProductHelper.tsx
@@ -22,11 +22,12 @@ const getLocalizedTitle = (product: ProductItem, locale: string): string => {
 };
 
 function ProductHelper({ items }: ProductListProps  ) {
-  if (!items || items.length === 0) {
-    return null; 
-  }
-  const t = useTranslations();
-  return (
+    const t = useTranslations();
+    
+    if (!items || items.length === 0) {
+      return null; 
+    }
+    return (
    <>
     <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-8">
       {items.map((item) => (
